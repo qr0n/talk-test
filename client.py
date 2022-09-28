a25c418bddf497dc88dfa35b623a463a2b55e3e8
Infinity Iron <77075118+qr0n@users.noreply.github.com>
2022-09-28 17:04:41 -0500

    Update client.py

--- client.py
+++ client.py
@@ -3,18 +3,16 @@ import httpx
 import json
 
 client_name = "Fr"
+server_url = ""
+chat_bg = ""
+bg = ""
 
-def config():
-    with open("config.json", "r") as E:
-        lE = json.load(E)
-        return lE
-
-username = f"{config()['user']} ({client_name})"
+username = f"Infinity Iron ({client_name})"
 
 class server:
     @staticmethod
     def return_message_list():
-        config_url = f"{config()['url']}"
+        config_url = server_url
         try:
             return httpx.get(f"{config_url}/api/messages").text
         except httpx.ConnectTimeout:
@@ -22,7 +20,7 @@ class server:
     
     @staticmethod
     def send_message(message):
-        config_url = f"{config()['url']}"
+        config_url = server_url
         httpx.post(f"{config_url}/api/send", headers={"user" : username, "content" : message})
     
     
@@ -30,17 +28,17 @@ class server:
 class client:
     @staticmethod
     def connect():
-        config_url = f"{config()['url']}"
+        config_url = server_url
         httpx.post(f"{config_url}/api/join", headers={"user" : username})
         return server.return_message_list()
 
 root = Tk()
 
-root.title(f"The Francium Client - {config()['url']}")
+root.title(f"The Francium Client - Pydroid Port")
 
 config_url = f"{config()['url']}"
 
-message_list = Label(root, text=client.connect(), background=config()["chat-bg"])
+message_list = Label(root, text=client.connect(), background=bg)
 entry = Entry(root, width=150)
 member_list = Label(root, text="Member list | off")
 
@@ -48,9 +46,9 @@ message_list.grid(row=1, sticky=W)
 entry.grid(row=5, sticky=SW)
 
 def mem_list():
-    if config()["member_list"] == "on":
+    if "on" == "on":
         member_list.grid(row=1, column=10, sticky=E)
-        member_list.config(text=httpx.get(f"{config()['url']}/api/members").text)
+        member_list.config(text=httpx.get(f"{server_url}/api/members").text)
         root.after(10000, mem_list)
         return
         
@@ -63,13 +61,13 @@ def update():
     root.after(1000, update)
 
 def send_message():
-        config_url = f"{config()['url']}"
+        config_url = server_url
         httpx.post(f"{config_url}/api/send", headers={"user" : username, "content" : entry.get()})
 
 send = Button(text="Send", command=send_message)
 send.grid(row=5, column=3, sticky=SW)
 
-root.configure(bg=config()["bg"])
+root.configure(bg=bg)
 mem_list()
 update()
 root.mainloop()
